2993a80d016d83900efd3174f4964120a195f634
Abeer Choubey <lotozipcorporation@gmail.com>
2025-05-17 14:45:07 +0530

    Add files via upload

---
+++ simple_calc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+float x,y;
+int main() {
+    cout << "This was made by Abeer Choubey \n";
+    cout << "Enter the first number: ";
+    cin >> x;
+    cout << "\nEnter the second number: ";
+    cin >> y;
+    cout << "\nAddition:" << x+y << "\nSubtraction:" << x-y << "\nMultipilication:" << x*y << "\n Division:" << x/y;
+    return 0;
+}
